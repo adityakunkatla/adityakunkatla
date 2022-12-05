66d201af8cbb2f1e4ad3a8f1acf02a820e4b4a6a
adityakunkatla <adityakunkatla987@gmail.com>
2022-12-05 16:23:25 +0530

    create a java file

--- file.java
+++ file.java
@@ -1,20 +1,15 @@
-import java.util.Scanner;
+import java.util.*;
 
-public class PrimeFactors {
-   public static void main(String args[]){
-      int number;
-      Scanner sc = new Scanner(System.in);
-      System.out.println("Enter a number ::");
-      number = sc.nextInt();
-     
-      for(int i = 2; i< number; i++) {
-         while(number%i == 0) {
-            System.out.println(i+" ");
-            number = number/i;
-         }
-      }
-      if(number >2) {
-         System.out.println(number);
-      }
-   }
-}																										
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        int x = 89; 
+        
+        int y = 43;
+        
+        int sum = x + y;
+        
+        System.out.println("Sum is " + sum);
+    }
+}
