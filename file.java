e0adb1cb6c900e33e8d8120c1372f8326b27d4ac
adityakunkatla <adityakunkatla987@gmail.com>
2022-12-05 16:07:39 +0530

    create a java file

--- file.java
+++ file.java
@@ -1,20 +1,3 @@
-import java.util.*;
-
-public class Main {
-    
-    public static void main(String[] args) {
-        
-        int x = 89; 
-        
-        int y = 43;
-        
-        int sum = x + y;
-        
-        System.out.println("Sum is " + sum);
-    }
-}
-
-
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
