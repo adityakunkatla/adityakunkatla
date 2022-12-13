aaf8d25c4bd66fcef2dbe9492b7200422d3c2c31
adityakunkatla <adityakunkatla987@gmail.com>
2022-12-13 15:14:11 +0530

    create java file

--- less.java
+++ less.java
@@ -2,12 +2,9 @@ public class Main {
 
   public static void main(String[] args) {
 
-    int count = 0, num = 0003452;
+    int count = 0, num = 123456;
 
-    while (num != 0) {
-      // num = num/10
-      num /= 10;
-      ++count;
+    for (; num != 0; num /= 10, ++count) {
     }
 
     System.out.println("Number of digits: " + count);
