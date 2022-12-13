1b792082d357a70bf9a12ba19430a350ba1dfc0e
root <root@ip-172-31-47-76.ap-south-1.compute.internal>
2022-12-13 09:23:13 +0000

    create a file

--- less.java
+++ less.java
@@ -1,5 +1,9 @@
-class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+class Main {
+  public static void main(String[] args) {
+
+    char c;
+
+    for(c = 'A'; c <= 'Z'; ++c)
+      System.out.print(c + " ");
     }
 }
